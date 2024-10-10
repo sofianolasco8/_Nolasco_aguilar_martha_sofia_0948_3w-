bb9c0466dc59b352636d7c32b334304797582412
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-10 10:35:34 -0700

    Add files via upload

--- pr_2_2do_parcial_funciones.py (4).py
+++ pr_2_2do_parcial_funciones.py (4).py
@@ -1,16 +1,20 @@
 print(" ")
 print("nolasco_aguilar_martha_sofia_0948_3w")
 print(" ")
-def es_vocal(caracter):#se crea una lista 
+def my_function(p1, p2):
+    coo1, coo1 = p1
+    coo2, coo2 = p2
+    # Calcula la diferencia de las coordenadas 
+    return (coo2 - coo1, coo2 - coo1)
 
-    vocales = "aeiouAEIOU"  #  vocales  minúsculas y mayúsculas
-    return caracter in vocales  # imprime True si es vocal y False si no 
+# indica al usuario que ingrese las coordenadas de los puntos
+print("ingesa las coordenadas del primer punto")
+coo1 = float(input("coordenada 1  "))
+coo1 = float(input("coordenada 2 "))
+print("Ingrese las coordenadas del segundo punto:")
+coo2 = float(input("coordenada 1 "))
+coo2 = float(input("coordenada 2 "))
 
-# indica al usuario ingresar  una letra
-letra = input("Ingresa una letra ")
-
-# Verificar si es una vocal 
-if es_vocal(letra):
-    print(True)#indica que  es una vocal
-else:
-    print(False)#IDICA QUE NO ES UNA VOCAL
+# mostrar el resultado
+resultado = my_function((coo1, coo1), (coo2 , coo2))
+print(f"La distancia dirigida entre los puntos es: {resultado}")
